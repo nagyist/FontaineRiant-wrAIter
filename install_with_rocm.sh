3317862af8e09f3579e67ee098feff2fbbe6447c
FontaineRiant <antoine.friant@gmail.com>
2025-02-23 13:00:25 +0100

    ROCm install script update

--- install_with_rocm.sh
+++ install_with_rocm.sh
@@ -2,10 +2,11 @@ set -e
 python3.12 -m venv venv
 source ./venv/bin/activate
 pip install --upgrade pip
-pip install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/rocm5.7
 pip install whisper-mic==1.4.2 sentencepiece inquirerpy coqui-tts pygame pysbd diffusers accelerate gguf
-pip install --upgrade transformers==4.46.1
+pip install transformers torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/rocm6.2
 # quantization
 # pip install --extra-index-url https://download.pytorch.org/whl/rocm6.1 torch==2.4.1+rocm6.1 torchaudio==2.4.1+rocm6.1 torchvision==0.19.1+rocm6.1
 # pip install --force-reinstall --no-deps 'https://github.com/bitsandbytes-foundation/bitsandbytes/releases/download/continuous-release_multi-backend-refactor/bitsandbytes-0.44.1.dev0-py3-none-manylinux_2_24_x86_64.whl'
 read -sn 1 -p "Press any key to continue.."
+
+
