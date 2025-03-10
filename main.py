20ac55fa99e78231bc84b568039a034e796ce213
FontaineRiant <antoine.friant@gmail.com>
2025-03-10 15:53:06 +0100

    preserve current input when saving/using thesaurus while edting past events

--- main.py
+++ main.py
@@ -222,14 +222,14 @@ class Game:
         while True:
             try:
                 self.pprint()
-                default_input = self.redo_history[0] if self.redo_history else default_input
+                default_input = self.redo_history[0] if self.redo_history and not default_input else default_input
                 if self.redo_history:
                     self.status_text += f'\n  [{len(self.story.events)}/{len(self.redo_history) + len(self.story.events) - 1}]'
                 inquirer_prompt = inquirer.text(message='', qmark='', amark='', raise_keyboard_interrupt=False,
                                                 mandatory=False, default=default_input, multiline=True,
                                                 long_instruction=self.status_text,
                                                 instruction=' ', keybindings={'answer': [{'key': ['enter', 'enter']}]})
-                self.status_text = 'Enter twice to send, Ctrl-L for commands'
+                self.status_text = '  Enter twice to send, Ctrl-L for commands'
                 default_input = ''
 
                 # Declare keybinds
